4efafdbbc570e8047e0cc0c7b2e40ec9a382eee5
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2024-09-17 20:26:34 -0600

    JavaScrip

---
+++ Javascript/01-tipos/01-variables.js
@@ -0,0 +1,15 @@
+UperCamelCase , camelCase , snake_case
+//UperCamelCase es para que todas las variables empiezen con una mayuscula segun su nombre
+//camelCase es para que puedas escribirla sin que esta tenga que estar ña primera en mayuscula pero la primera si
+// snake_case es para que no tengas que usar espacios y en vez xde eso acabes usando un guion bajo
+let nombre = 'Hola mundo';
+let NombreCompleto;// esta es una variable tipo UperCamelCase
+let nombreCompleto;
+let nombre_completo;//esta  es la mas comun a la hora de usar javascript
+console.log(NombreCompleto);
+
+let apellido;
+appellido = 'Schurman'
+
+//para hacer una variable en java script lo que hace es usar el termino let por que var se utiliza para ptra cosa
+// asi mismo como java script tiene case sensitive al tener el mismo nombre pero cmabia los caracteres la toma como una variable diferente
---
+++ Javascript/01-tipos/03-constantes.js
@@ -0,0 +1,6 @@
+let nombre = "Hola Mundo";
+nombre = "Chanchito feliz";// se puede cambiar el nombre de las variables mas adelante pero no se caostumbra
+
+//unabuena practica es no cambiar los valores de las variables ya que esto podria confundir o romper el codigo
+
+console.log(nombre);
